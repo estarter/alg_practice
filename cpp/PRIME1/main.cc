79a64c7610317ec63554ec3a97687764cbbf74bf
estarter <estarter@gmail.com>
2016-04-19 23:48:09 +0200

    write tests inside main.cc

--- cpp/PRIME1/main.cc
+++ cpp/PRIME1/main.cc
@@ -3,26 +3,45 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 using std::cout; using std::endl; using std::cin;
 
 
-bool is_divided_by(const int& number, const std::vector<int> & primes, const int& until) {
+bool is_divided_by(const int& number, const std::vector<int> & primes) {
     for (int p : primes) {
-        // if (p > until) return false;
         if (number == p) return false;
         if (number % p == 0) return true;
     }
     return false;
 }
 
+TEST(is_divided_by, test) {
+    std::vector<int> v = {2, 3};
+    ASSERT_FALSE(is_divided_by(2, v));
+    ASSERT_FALSE(is_divided_by(3, v));
+    ASSERT_FALSE(is_divided_by(5, v));
+
+    ASSERT_TRUE(is_divided_by(4, v));
+    ASSERT_TRUE(is_divided_by(6, v));
+}
+
 std::vector<int> prime_numbers(const int& to) {
     std::vector<int> result;
     for (int i = 2; i <= to; i++) {
-        if (!is_divided_by(i, result, to)) {
+        if (!is_divided_by(i, result)) {
             result.push_back(i);
         }
     }
     return result;
+}
+TEST(prime_numbers, test) {
+    std::vector<int> v {2,3};
+    ASSERT_THAT(v, ::testing::ContainerEq(prime_numbers(3)));
+    ASSERT_THAT(v, ::testing::ContainerEq(prime_numbers(4)));
+    v.push_back(5);
+    ASSERT_THAT(v, ::testing::ContainerEq(prime_numbers(5)));
+    ASSERT_THAT(v, ::testing::ContainerEq(prime_numbers(6)));
 }
 
 std::vector<int> prime_numbers_force(int from, int to) {
@@ -41,14 +60,30 @@ std::vector<int> prime_numbers(int from, int to) {
     std::vector<int> result;
     // std::copy(begin(first_primes), )
     for (int i = from; i <= to; i++) {
-        if (!is_divided_by(i, first_primes, from) && i > 1) {
+        if (i > 1 && !is_divided_by(i, first_primes)) {
             result.push_back(i);
         }
     }
     return result;
 }
+TEST(prime_numbers, interval_test) {
+    std::vector<int> v{2, 3, 5, 7};
+    ASSERT_THAT(v, ::testing::ContainerEq(prime_numbers(1, 10)));
+    v = {11, 13, 17, 19};
+    ASSERT_THAT(v, ::testing::ContainerEq(prime_numbers(10, 20)));
+    v = {31, 37, 41, 43, 47};
+    ASSERT_THAT(v, ::testing::ContainerEq(prime_numbers(30, 50)));
+}
 
-int main() {
+
+int main(int argc, char **argv) {
+    if (argc == 2 && std::string(*(argv+1)) == "test") {
+        ::testing::GTEST_FLAG(throw_on_failure) = true;
+        ::testing::InitGoogleMock(&argc, argv);
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+    }
+
     int test_amount = 0;
     cin >> test_amount;
     while (test_amount-- > 0) {
