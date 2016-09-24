ea279eae2da94663d5fcb7cac1bdbfe772561667
estarter <estarter@gmail.com>
2016-09-24 21:13:13 +0200

    implement O(NlogN) solution to find longest increasing subseq

--- src/main/java/longest_inc_subseq/Main.java
+++ src/main/java/longest_inc_subseq/Main.java
@@ -6,7 +6,68 @@ import java.util.*;
  * @author Alexey Merezhin
  */
 public class Main {
-    public Integer[] findLongestIncSubseq(Integer[] seq) {
+
+    // O(N*logN)
+    public static List<Integer> longest_increasing_subsequence(List<Integer> seq) {
+        List<int[]> min = new ArrayList<int[]>(seq.size());
+        min.add(new int[]{seq.get(0), 0});
+        List<Integer> prev = new ArrayList<Integer>(seq.size());
+        prev.add(-1);
+        for (int i = 1; i < seq.size(); i++) {
+            int val = seq.get(i);
+
+            int idx = getPos(min, val);
+
+            if (idx == min.size()) {
+                min.add(new int[]{val, i});
+            } else {
+                if (val < min.get(idx)[0]) {
+                    min.get(idx)[0] = val;
+                    min.get(idx)[1] = i;
+                }
+            }
+            if (idx == 0) {
+                prev.add(-1);
+            } else {
+                prev.add(min.get(idx-1)[1]);
+            }
+
+        }
+
+        List<Integer> result = new ArrayList<Integer>();
+        int idx = min.get(min.size()-1)[1];
+        while (idx >= 0) {
+            result.add(seq.get(idx));
+            idx = prev.get(idx);
+        }
+
+        Collections.reverse(result);
+        return result;
+    }
+
+    static int getPos(List<int[]> min, int value) {
+        int lo = 0;
+        int hi = min.size() - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (value == min.get(mid)[0]) {
+                return mid;
+            }
+            if (value > min.get(hi)[0]) return hi + 1;
+            if (value < min.get(lo)[0]) return lo;
+
+            if (value > min.get(mid)[0]) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        throw new RuntimeException("failed to pos an element");
+    }
+
+    // O(N^2)
+    public static Integer[] findLongestIncSubseq(Integer[] seq) {
         int[] maxLengths = new int[seq.length];
         int totalMax = 0;
         for (int i = 0; i < seq.length; i++) {
--- src/test/java/longest_inc_subseq/MainTest.java
+++ src/test/java/longest_inc_subseq/MainTest.java
@@ -2,8 +2,12 @@ package longest_inc_subseq;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -12,10 +16,49 @@ import static org.junit.Assert.*;
 public class MainTest {
     @Test
     public void testSeq() throws Exception {
-        Main main = new Main();
-        Integer[] seq = main.findLongestIncSubseq(new Integer[]{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15});
-        System.out.println(Arrays.toString(seq));
+        Integer[] seq = Main.findLongestIncSubseq(new Integer[]{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15});
         assertArrayEquals(new Integer[]{0, 2, 6, 9, 11, 15}, seq);
 
+        seq = Main.findLongestIncSubseq(new Integer[]{2984, 1743, 8889, 3708, 8747, 6831, 2570, 6473, 9962, 241});
+        assertArrayEquals(new Integer[]{1743, 2570, 6473, 9962}, seq);
+    }
+
+    @Test
+    public void testSeq2() throws Exception {
+        List<Integer> seq;
+        seq = Main.longest_increasing_subsequence(Arrays.asList(16, 3, 5, 19, 10, 14, 12, 0, 15));
+        assertThat(seq, is(Arrays.asList(3, 5, 10, 12, 15)));
+
+        seq = Main.longest_increasing_subsequence(Arrays.asList(14));
+        assertThat(seq, is(Arrays.asList(14)));
+
+        seq = Main.longest_increasing_subsequence(Arrays.asList(10, 8, 6, 4, 2, 0));
+        assertThat(seq, is(Arrays.asList(0)));
+
+        seq = Main.longest_increasing_subsequence(Arrays.asList(2984, 1743, 8889, 3708, 8747, 6831, 2570, 6473, 9962, 241));
+        assertThat(seq, is(Arrays.asList(1743, 2570, 6473, 9962)));
+    }
+
+    @Test
+    public void crossTest() {
+        int testSize = 100;
+        for (int k = 0; k < 5; k++) {
+            Random randomGenerator = new Random();
+            List<Integer> input = new ArrayList<>(testSize);
+            for (int i = 0; i < testSize; i++) {
+                input.add(randomGenerator.nextInt(Math.min(testSize * 1000, Integer.MAX_VALUE)));
+            }
+
+            long startTime = System.nanoTime();
+            Integer[] result1 = Main.findLongestIncSubseq(input.toArray(new Integer[]{}));
+            System.out.println("first  " + (System.nanoTime() - startTime));
+
+            startTime = System.nanoTime();
+            List<Integer> result2 = Main.longest_increasing_subsequence(input);
+            System.out.println("second " + (System.nanoTime() - startTime));
+
+            System.out.println(input.toString());
+            assertThat(input.toString(), result2, is(Arrays.asList(result1)));
+        }
     }
 }
